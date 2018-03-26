feb4eb4caaaecf790e1059f3af5465faa5182b45
zhangchao <dzzchao@163.com>
2018-03-26 00:35:40 -0700

    supplement

--- README.md
+++ README.md
@@ -32,3 +32,39 @@ imageView.startAnimation(animation);
 
 
 ## 属性动画
+
+- ObjectAnimator
+
+ObjectAnimator animator = ObjectAnimator.ofFloat(imageView,"translationY",300);
+animator.setDuration(1000);
+animator.start();
+```
+- PropertyValuesHolder 的使用，多个动画一起执行
+```
+PropertyValuesHolder valuesHolder1 = PropertyValuesHolder.ofFloat("translationX", 300);
+PropertyValuesHolder valuesHolder2 = PropertyValuesHolder.ofFloat("translationY", 300);
+ObjectAnimator.ofPropertyValuesHolder(imageView,valuesHolder1, valuesHolder2).setDuration(1000).start();
+```
+- ValueAnimator
+```
+ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+valueAnimator.addUpdateListener(
+        new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                (textView).setText(animation.getAnimatedValue() + "");
+            }
+        });
+valueAnimator.setDuration(3000);
+valueAnimator.start();
+```
+- AnimatorSet 可以实现更为精确的顺序控制
+```
+AnimatorSet set = new AnimatorSet();
+set.setDuration(500);
+set.setInterpolator(new BounceInterpolator());
+set.playTogether(animator0, animator1, animator2, animator3, animator4);
+set.start();
+```
+参考博客：
+https://blog.csdn.net/harvic880925/article/details/50995268
--- app/src/main/java/com/dzzchao/learnanimation/MainActivity.java
+++ app/src/main/java/com/dzzchao/learnanimation/MainActivity.java
@@ -1,15 +1,17 @@
 package com.dzzchao.learnanimation;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
 import android.view.animation.RotateAnimation;
 import android.view.animation.ScaleAnimation;
-import android.view.animation.TranslateAnimation;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,9 +40,45 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.btn_translate)
     void translate() {
-        TranslateAnimation animation = new TranslateAnimation(0, 200, 0, 400);
-        animation.setDuration(1000);
-        imageView.startAnimation(animation);
+//        TranslateAnimation animation = new TranslateAnimation(0, 200, 0, 400);
+//        animation.setDuration(1000);
+//        imageView.startAnimation(animation);
+        //属性动画写法
+        ObjectAnimator animator = ObjectAnimator.ofFloat(imageView, "translationY", 300);
+        animator.setDuration(1000);
+        animator.start();
+        //动画监听
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationCancel(Animator animation) {
+                super.onAnimationCancel(animation);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+                super.onAnimationRepeat(animation);
+            }
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+            }
+
+            @Override
+            public void onAnimationPause(Animator animation) {
+                super.onAnimationPause(animation);
+            }
+
+            @Override
+            public void onAnimationResume(Animator animation) {
+                super.onAnimationResume(animation);
+            }
+        });
     }
 
     @OnClick(R.id.btn_scale)
@@ -51,13 +89,29 @@ public class MainActivity extends AppCompatActivity {
         imageView.startAnimation(animation);
     }
 
+    @BindView(R.id.text_view)
+    TextView textView;
+
+    @OnClick(R.id.button_value_animator)
+    void valueAnimator() {
+        ValueAnimator valueAnimator = ValueAnimator.ofInt(0, 100);
+        valueAnimator.addUpdateListener(
+                new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                        (textView).setText(animation.getAnimatedValue() + "");
+                    }
+                });
+        valueAnimator.setDuration(3000);
+        valueAnimator.start();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-
 
     }
 }
